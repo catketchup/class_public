9cc98a5ad96e49f0269c190d683dbaa027e8c11b
Hongbo <hoc34@pitt.edu>
2021-07-29 14:29:15 -0400

    backup, rotation module

---
+++ include/rotation.h
@@ -0,0 +1,61 @@
+struct rotation {
+	short has_rotated_cls;
+
+	int has_tt;
+	int has_te;
+	int has_tb;
+	int has_ee;
+	int has_bb;
+	int has_eb;
+	int has_aa; /**< do we want \f$ C_l^{\alpha\alpha}\f$? (\f$ \alpha \f$ = rotation field) */
+
+	int l_unrotated_max;
+	int l_rotated_max;
+
+
+
+
+
+};
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+	int rotation_cl_at_l(
+		struct rotation * pro,
+		int l,
+		double * cl_rotated
+		);
+
+	int rotation_init(
+		struct precision * ppr,
+		struct perturbations * ppt,
+		struct harmonic * phr,
+		struct fourier * pfo,
+		struct rotation * pro
+		);
+
+	int rotation_free(struct rotation * pro
+		);
+
+	int rotation_indices(
+		struct precision * ppr,
+		struct harmonic * phr,
+		struct rotation * pro
+		);
+
+	/* int rotation_rotated_cl_tt( */
+	/* 	); */
+
+	
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ source/rotation.c
@@ -0,0 +1,43 @@
+#include "rotation.h"
+#include <time.h>
+
+
+int rotation_cl_at_l(
+	struct rotation * pro,
+	int l,
+	double * cl_rotated
+	) {
+}
+
+int rotation_init(
+	struct precision * ppr,
+	struct perturbations * ppt,
+	struct harmoic * phr,
+	struct fourier * pfo,
+	struct rotation * pro
+	) {
+
+
+
+
+}
+
+
+int rotation_free(
+	struct rotation * pro
+	) {
+
+}
+
+int rotaion_indices(
+	struct precision * ppr,
+	struct harmonic * phr,
+	struct rotation * pro
+	){
+
+
+}
+
+
+int rotation_rotated_cl_te(
+	double *)
